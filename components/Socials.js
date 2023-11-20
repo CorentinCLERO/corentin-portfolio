41e309c93c07358b42effd46696f87eedd0bcb52
CorentinCLERO <corentin.clero@epsi.fr>
2023-11-20 17:41:29 +0100

    page home

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,30 @@
+import Link from "next/link";
+
+import { RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiBehanceLine, RiPinterestLine } from 'react-icons/ri'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-all">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-all">
+        <RiInstagramLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-all">
+        <RiFacebookLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-all">
+        <RiDribbbleLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-all">
+        <RiBehanceLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-all">
+        <RiPinterestLine />
+      </Link>
+    </div>
+  )
 };
 
 export default Socials;
