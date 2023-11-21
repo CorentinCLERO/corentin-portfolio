68cf90b2edfed15daf6a0a4a96bdd4d1ea2ccf8e
CorentinCLERO <corentin.clero@epsi.fr>
2023-11-21 11:07:11 +0100

    build

--- components/Circles.js
+++ components/Circles.js
@@ -7,6 +7,7 @@ const Circles = () => {
       width={260}
       height={200}
       className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10"
+      alt=""
     />
   </div>;
 };
